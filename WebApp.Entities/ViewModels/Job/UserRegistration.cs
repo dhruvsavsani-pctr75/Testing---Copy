b4374cef7c87f2c35fac307666679988b6e97700
dhruvsavsani-pctr75 <tatva.pctr75@outlook.com>
2025-05-15 20:58:23 +0530

    Update UserRegistration.cs

--- WebApp.Entities/ViewModels/Job/UserRegistration.cs
+++ WebApp.Entities/ViewModels/Job/UserRegistration.cs
@@ -4,19 +4,22 @@ namespace WebApp.Entities.ViewModels.Job;
 
 public class UserRegistration
 {
-    [Required(ErrorMessage = "Username is required.")]
-    public string Username { get; set; } = "";
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; } = "";
 
-    [Required(ErrorMessage = "Password is required")]
-    [MinLength(8, ErrorMessage = "Minimum password length is 8")]
-    [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-            ErrorMessage = "Password must be at least 8 characters long, contain one uppercase letter, one number, and one special character.")]
-    public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Minimum password length is 8")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+                ErrorMessage = "Password must be at least 8 characters long, contain one uppercase letter, one number, and one special character.")]
+        public string Password { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Password is required")]
-    [MinLength(8, ErrorMessage = "Minimum password length is 8")]
-    [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-            ErrorMessage = "Password must be at least 8 characters long, contain one uppercase letter, one number, and one special character.")]
-    [Compare("Password", ErrorMessage = "Password and confirm password do not match.")]
-    public string ConfirmPassword { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Minimum password length is 8")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+                ErrorMessage = "Password must be at least 8 characters long, contain one uppercase letter, one number, and one special character.")]
+        [Compare("Password", ErrorMessage = "Password and confirm password do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    
+        [Required(ErrorMessage = "Role is required.")]
+        public string RoleId { get; set; }
 }
